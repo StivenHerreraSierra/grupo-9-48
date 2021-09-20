e0d61af77d615a6bf2769b2563d14754174a5330
StivenHerreraSierra <stiven.herreras@uqvirtual.edu.co>
2021-09-19 23:50:42 -0500

    Ajustes al menu y migracion del panel de administracion

---
+++ frontend/src/components/Card.vue
@@ -0,0 +1,50 @@
+<template>
+<v-hover v-slot="{ hover }">
+  <v-card class="mx-auto" color="grey lighten-4" max-width="600" height="250px">
+    <v-img :aspect-ratio="16 / 9" :src="document.image" height="250px">
+      <v-expand-transition>
+        <div
+          v-if="hover"
+          class="
+            d-flex
+            transition-fast-in-fast-out
+            black
+            darken-2
+            v-card--reveal
+            white--text
+            flex-column
+            align-start
+            justify-space-between
+            pa-2
+          "
+          style="height: 100%;"
+        >
+          <p class="text-h6">{{ document.title }}</p>
+          <p>Last open<br />{{ document.last_open }}</p>
+        </div>
+      </v-expand-transition>
+    </v-img>
+  </v-card>
+  </v-hover>
+</template>
+
+<script>
+export default {
+  props: {
+    document: Object,
+  },
+};
+</script>
+
+<style>
+
+.v-card--reveal {
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  opacity: .5;
+  position: absolute;
+  width: 100%;
+}
+
+</style>
--- frontend/src/views/AdminPanel.vue
+++ frontend/src/views/AdminPanel.vue
@@ -1,16 +1,63 @@
 <template>
-  <v-container>
+  <v-layout class="dark-background white--text">
     <Menu :content="menu_content"></Menu>
-    <v-container><h1>Panel Admin</h1></v-container>
-  </v-container>
+
+    <v-container fluid>
+      <h1>Your documents</h1>
+      <v-container class="mb-6" fluid>
+        <v-layout align-center justify-space-between>
+          <h2>Recents</h2>
+          <hr class="separador" />
+        </v-layout>
+
+        <v-row>
+          <v-col v-for="n in 5" :key="n">
+            <Card :document="document"></Card>
+          </v-col>
+        </v-row>
+      </v-container>
+
+      <v-container fluid>
+        <v-layout align-center justify-space-between>
+          <h2>All</h2>
+          <hr class="separador" />
+        </v-layout>
+
+        <v-row no-gutters justify="end">
+          <v-col cols="4">
+            <v-text-field
+              v-model="searched_title"
+              append-icon="mdi-send"
+              filled
+              clear-icon="mdi-close-circle"
+              clearable
+              placeholder="Document title"
+              type="text"
+              @click:append="search"
+              @click:clear="clearField"
+              dark
+            ></v-text-field>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col v-for="n in 24" :key="n" cols="3">
+            <Card :document="document"></Card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-container>
+  </v-layout>
 </template>
 
 <script>
 import Menu from "../components/Menu.vue";
+import Card from "../components/Card.vue";
 
 export default {
   components: {
     Menu,
+    Card,
   },
   data() {
     return {
@@ -38,10 +85,51 @@ export default {
           },
         ],
       },
+
+      document: {
+        image: "https://www.dliflc.edu/wp-content/uploads/2018/11/book.jpg",
+        title: "Document",
+        last_open: "Today",
+      },
+
+      searched_title: "",
+      marker: true,
     };
+  },
+  methods: {
+    search() {
+      console.log("Buscando:", this.searched_title);
+    },
+    clearField() {
+      this.searched_title = "";
+    },
   },
 };
 </script>
 
 <style>
+h1 {
+  font-size: 40px !important;
+  font-weight: normal;
+}
+h2 {
+  font-size: 28px !important;
+  font-weight: normal;
+}
+.container {
+  margin: 0;
+  width: 100% !important;
+}
+.dark-background {
+  background-color: #1c1f20;
+}
+.separador {
+  width: 90%;
+  border: none;
+  border-top-width: 1px;
+  border-top-style: solid;
+  border-top-color: rgb(255, 255, 255);
+  color: rgb(255, 255, 255);
+  background-color: rgb(255, 255, 255);
+}
 </style>
