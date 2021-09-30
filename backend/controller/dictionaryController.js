31c5edac185584f2a9653fd716cdb2a76b23fa6c
StivenHerreraSierra <stiven.herreras@uqvirtual.edu.co>
2021-09-30 15:23:52 -0500

    Ajuste en el timeout de llamado a la api OwlBot

--- backend/controller/dictionaryController.js
+++ backend/controller/dictionaryController.js
@@ -3,9 +3,10 @@ const OwlBot = require("owlbot-js");
 module.exports = class Dictionary {
     static async search(req, res) {
         try {
-            const cliente = OwlBot(process.env.OWL_TOKEN);
+            const cliente = OwlBot(process.env.OWL_TOKEN, 25000);
 
             const word = req.params.word;
+            
             cliente.define(word)
                 .then(result => res.status(200).json(result));
         } catch(err) {
